52290645dfc0aebbfbaa2abee9cdc2f4116ed247
muh_fahmi <fahmipradana4@gmail.com>
2022-06-11 11:31:44 +0800

    add script.js

---
+++ script.js
@@ -0,0 +1,93 @@
+let prevNumber = "";
+let calculationOperator = "";
+let currentNumber = "0";
+const calculatorScreen = document.querySelector(".calculator-screen");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equalSign = document.querySelector(".equal-sign");
+const clearBtn = document.querySelector(".all-clear");
+const decimal = document.querySelector(".decimal");
+
+const updateScreen = (number) => {
+    calculatorScreen.value = number;
+};
+
+const inputNumber = (number) => {
+    if (currentNumber === "0") {
+        currentNumber = number;
+    } else {
+        currentNumber += number;
+    }
+};
+
+const inputOperator = (operator) => {
+    if (calculationOperator === "") {
+        prevNumber = currentNumber;
+    }
+    console.log(prevNumber);
+    calculationOperator = operator;
+    currentNumber = "0";
+};
+
+const calculate = () => {
+    let result = "";
+    switch (calculationOperator) {
+        case "+":
+            result = parseFloat(prevNumber) + parseFloat(currentNumber);
+            break;
+        case "-":
+            result = parseFloat(prevNumber) - parseFloat(currentNumber);
+            break;
+        case "*":
+            result = parseFloat(prevNumber) * parseFloat(currentNumber);
+            break;
+        case "/":
+            result = parseFloat(prevNumber) / parseFloat(currentNumber);
+            break;
+        default:
+            return;
+    }
+    currentNumber = result;
+    calculationOperator = "";
+};
+
+const clearAll = () => {
+    prevNumber = "";
+    calculationOperator = "";
+    currentNumber = "0";
+};
+
+const inputDecimal = (dot) => {
+    if (currentNumber.includes(".")) {
+        return;
+    }
+    currentNumber += dot;
+};
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (event) => {
+        inputNumber(event.target.value);
+        updateScreen(currentNumber);
+    });
+});
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        inputOperator(event.target.value);
+    });
+});
+
+equalSign.addEventListener("click", () => {
+    calculate();
+    updateScreen(currentNumber);
+});
+
+clearBtn.addEventListener("click", () => {
+    clearAll();
+    updateScreen(currentNumber);
+});
+
+decimal.addEventListener("click", (event) => {
+    inputDecimal(event.target.value);
+    updateScreen(currentNumber);
+});
